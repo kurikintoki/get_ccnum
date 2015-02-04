4a7328875192e6dfa43f292629cdb94a73546130
kurikintoki <karakuriuzumaki@gmail.com>
2015-02-04 23:26:26 +0900

    switch the place of cc_id and subs_id

--- get_ccnum.sh
+++ get_ccnum.sh
@@ -6,6 +6,6 @@ do
     card=$line
     IFS=","
     ARRAY=($card)
-    /usr/bin/curl  -k -d "method=decrypt&subs_id=${ARRAY[0]}&cc_id=${ARRAY[1]}&cc_no=${ARRAY[2]}" https://192.168.2.5/dec_enc.php
+    /usr/bin/curl  -k -d "method=decrypt&cc_id=${ARRAY[0]}&subs_id=${ARRAY[1]}&cc_no=${ARRAY[2]}" https://192.168.2.5/dec_enc.php
     echo ""
 done < $1 
